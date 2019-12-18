81b30e46adfcf5aaa7f594812f8e5502b46614cf
Aron Tolentino <arontolentino@outlook.com>
2019-12-17 23:51:33 -0500

    Implement basic state management

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -1,11 +1,44 @@
 import React from 'react';
 
-class Photo extends React.Component {
-  render() {
-    const post = this.props.post;
+function Photo(props) {
+  const post = props.post;
 
-    return <figure className="figure"></figure>;
-  }
+  return (
+    <figure className="figure">
+      <img className="photo" src={post.imageLink} alt={post.description} />
+      <figcaption>
+        <p>{post.description}</p>
+      </figcaption>
+      <div className="button-container">
+        <button
+          className="remove-button"
+          onClick={() => {
+            props.onRemovePhoto(post);
+          }}
+        >
+          Remove
+        </button>
+      </div>
+    </figure>
+  );
 }
+
+// class Photo extends React.Component {
+//   render() {
+//     const post = this.props.post;
+
+//     return (
+//       <figure className="figure">
+//         <img className="photo" src={post.imageLink} alt={post.description} />
+//         <figcaption>
+//           <p>{post.description}</p>
+//         </figcaption>
+//         <div className="button-container">
+//           <button className="remove-button">Remove</button>
+//         </div>
+//       </figure>
+//     );
+//   }
+// }
 
 export default Photo;
